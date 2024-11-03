c0fffd1c4a5a152858f995e4881961e9a29cdf07
ahmedFarhat80 <Ahmed8051741@gmail.com>
2024-11-03 21:42:37 +0200

    Multiply Assignment 10

---
+++ Assets/code/Multiply.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiply : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int Multiplys(int a, int b)
+    {
+        return a * b;
+    }
+
+    void Start()
+    {
+        // int number = 5;
+        for (int i = 1; i <= 10; i++)
+        {
+            int number = i;
+            for (int j = 1; j <= 10; j++)
+            {
+                int result = Multiplys(number, j);
+                Debug.Log("Multiplication table for the number  " + $"{number} x {j} = {result}");
+            }
+        }
+
+
+    }
+
+
+}
